2621eb757765241234eb37d11a5c260cb0ecae89
Matthew Adaro <117700776+MatthewAdaro@users.noreply.github.com>
2023-10-25 10:09:57 +0800

    Initial Commit

---
+++ src/main/java/org/ssglobal/training/codes/TeamCityActivityApplication.java
@@ -0,0 +1,13 @@
+package org.ssglobal.training.codes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamCityActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamCityActivityApplication.class, args);
+	}
+
+}
